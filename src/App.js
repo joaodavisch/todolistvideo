fad7090c4fc84500a92889b62656bf8920e7241c
joaodavisch <joaodavi@scheidemantel.com.br>
2022-05-20 12:13:57 -0300

    FINISH

--- src/App.js
+++ src/App.js
@@ -6,23 +6,123 @@ const API = "http://localhost:5000"
 
 
 function App() {
-const [title, setTitle] = useState("")
-const [time, setTime] = useState("")
-const [todos, setTodos] = useState([])
-const [loading, setLoading] = useState(false)
+  const [title, setTitle] = useState("")
+  const [time, setTime] = useState("")
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(false)
+
+
+  useEffect(() => {
+
+    const loadData = async () => {
+      setLoading(true)
+
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => console.log(err));
+
+      setLoading(false)
+      setTodos(res);
+    }
+
+    loadData();
+  }, [])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    }
+
+
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+
+    setTodos((prevState) => [...prevState, todo]);
+    setTitle("");
+    setTime("");
+  };
+
+  const handleDelete = async (id) => {
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    })
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+  }
+
+  const handleEdit = async (todo) => {
+    todo.done = !todo.done;
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    setTodos((prevState) => prevState.map((t) => (t.id === data.id ? (t = data) : t))
+    );
+  }
+
+  if (loading) {
+    return <p>Carregando caralho</p>
+  }
 
 
   return (
-    <div  className="App">
+    <div className="App">
       <div className="todo-header">
         <h1>react todo</h1>
       </div>
       <div className="form-todo">
-        <h1>formulario</h1>
+        <h2>Insira a sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor="title">o que você vai fazer? </label>
+            <input type="text"
+              name="title"
+              placeholder="Título da tarefa"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+              required />
+          </div>
+          <div className='form-control'>
+            <label htmlFor="time">Duração:</label>
+            <input type="text"
+              name="time"
+              placeholder="Tempo em horas caralho"
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""}
+              required />
+          </div>
+          <input type="submit" value="Criara tarefa" />
+        </form>
       </div>
       <div className="list-todo">
         <h2>Listas de tarefas</h2>
         {todos.length === 0 && <p>Não a tarefas</p>}
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
+            <p>Duração: {todo.time}</p>
+            <div className="actions">
+              <span onClick={() => handleEdit(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
